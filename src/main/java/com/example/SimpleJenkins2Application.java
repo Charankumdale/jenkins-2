ee9014f9cc586bcc723d09e51066d446e930b9f1
Charankumdale <ckumdale27051999@gmail.com>
2022-11-10 10:27:53 +0530

    project Add

---
+++ src/main/java/com/example/SimpleJenkins2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJenkins2Application.class, args);
+	}
+
+}
